7a63050a8afff87b5543bea1609ffea60883766d
Gustav-hdoliveira <162641711+Gustav-hdoliveira@users.noreply.github.com>
2024-09-12 21:51:40 -0300

    Create SEAUTO_ServiçoTP.ts

---
+++ SEAUTO_ServiçoTP.ts
@@ -0,0 +1,10 @@
+class OcorrenciaTP {
+    id_SVTP: number
+    nome_ocorrencia: string
+    
+    constructor(id: number, nome: string){
+        this.id_SVTP = id
+        this.nome_ocorrencia = nome
+    }
+    
+}
