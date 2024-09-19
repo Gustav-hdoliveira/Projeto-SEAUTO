959bcff8732da457b747b27566ab0144a75dfe0d
Gustav-hdoliveira <162641711+Gustav-hdoliveira@users.noreply.github.com>
2024-09-18 21:50:25 -0300

    Add files via upload

---
+++ SEAUTO_Rede.ts
@@ -0,0 +1,9 @@
+import { Usuario } from "./SEAUTO_Usuario";
+import { Fonte } from "./SEAUTO_OrigemServiço";
+class Rede {
+    usuario: Usuario[]
+    fonte: Fonte[]
+    constructor() {
+        
+    }
+}
