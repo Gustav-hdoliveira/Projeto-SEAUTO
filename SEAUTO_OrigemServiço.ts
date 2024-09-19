ac1caea558ce7343e14198d9db0668faf5173f64
Gustav-hdoliveira <162641711+Gustav-hdoliveira@users.noreply.github.com>
2024-09-19 16:26:51 -0300

    Update SEAUTO_OrigemServiço.ts

--- SEAUTO_OrigemServiço.ts
+++ SEAUTO_OrigemServiço.ts
@@ -1,16 +1,32 @@
 import { OcorrenciaTP } from "./SEAUTO_OcorrenciaTP";
+import { Rede } from "./SEAUTO_Rede";
 export class Fonte{
     id_fonte: number
     nome_fonte: string
     telefone: number
-    id_tipo_Ocorrencia: OcorrenciaTP
+    id_tipo_Ocorrencia: number
 
-    constructor(id: number, nome: string, telefone: number, idTipoOC: OcorrenciaTP){
+    constructor(id: number, nome: string, telefone: number, idTipoOC: number){
         this.id_fonte = id
         this.nome_fonte = nome
         this.telefone = telefone
         this.id_tipo_Ocorrencia = idTipoOC
     }
 
-    setFonte()
+    setFonte(){
+        
+    }
+
+    getIDfonte():number{
+        return this.id_fonte
+    }
+
+    getNomeFonte():string{
+        return this.nome_fonte
+    }
+
+    getTelefone():number{
+        return this.telefone
+    }
+
 }
